fc246c8429f899de57bd59ff6b6a66fb7056e36e
Tarllis Sales <tarllis@gmail.com>
2025-11-13 23:16:10 -0300

    Commit Inicial

---
+++ calculadora/bin/calculadora.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+  List<String> operacoes = <String>["+", "-", "*", "/"];
+  String? entrada = "";
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+  void calcular() {
+    switch (operacao) {
+      case "+":
+        soma();
+
+      case "-":
+        subtracao();
+
+      case "*":
+        multiplicacao();
+
+      case "/":
+        divisao();
+        break;
+    }
+  }
+
+  void getOperacao() {
+
+    print("Digite uma operação");
+    entrada = stdin.readLineSync();
+    if(entrada != null){
+
+        if(operacoes.contains(entrada)) {
+            operacao = entrada!;
+        }
+
+    }
+}
+
+  print("Digite o primeiro valor");
+
+  entrada = stdin.readLineSync();
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada!);
+    }
+  }
+
+  getOperacao();
+
+  print("Digite o segundo valor");
+
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada!);
+    }
+  }
+
+  print("O resultado da operação é:");
+
+  calcular();
+}
